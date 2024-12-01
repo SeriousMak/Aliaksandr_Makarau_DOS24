5d6a49d77eb27ed4c8545442757bf1ec456ff640
SeriousMak <sashamkr@mail.ru>
2024-12-01 22:43:21 +0300

    feat: adding command read

--- Homework_Lesson11_BashShell_1/1.sh
+++ Homework_Lesson11_BashShell_1/1.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-#  Переменные
+# Получение параметров
+
+read -p "Введите название выводящего(output) файла:" outputfile
+echo $outputfile
+
+read -p "Введите имя каталога:" catalog
+echo $catalog
+
+read -p "Введите расширение файлов, которое вам необходимо:" extantion
+echo $extantion 
+
+# Переменные
 outputfile=$1
 catalog=$2
 extantion=$3
 
-#Поиск файлов с заданным расширением и вывод его в outputfile
+# Поиск файлов с заданным расширением и вывод его в outputfile
 find $catalog -type f -name $extantion > $outputfile
 
