4bb5a3d1a1dfa97b7e5d9f7066a8db3de390c8e0
SeriousMak <sashamkr@mail.ru>
2024-12-02 16:20:53 +0300

    add echo outputfile @deng4, Homework11_BashShell_1

--- Homework_Lesson11_BashShell_1/extension_search_list.sh
+++ Homework_Lesson11_BashShell_1/extension_search_list.sh
@@ -18,10 +18,11 @@ extansion=$3
 
 # Проверка, существует ли данный каталог
 if [ ! -d $catalog]; then
-    echo 'Ошибка: Данный каталог '$catalog' не существует'
+    echo "Ошибка: Данный каталог '$catalog' не существует"
     exit 1
 fi
 
 # Поиск файлов в заданном каталоге, с заданным расширением и вывод его в outputfile
 find $catalog -type f -name *$extansion > $outputfile
 
+echo "Список файлов с расширениями '$extansion' в файле '$outputfile'"
