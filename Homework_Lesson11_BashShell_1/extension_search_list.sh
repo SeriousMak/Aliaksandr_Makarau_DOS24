6ee1b9a7f9dc483592ab7d6aa11c3cb82635123a
SeriousMak <sashamkr@mail.ru>
2024-12-02 18:21:09 +0300

    fixing the script

--- Homework_Lesson11_BashShell_1/extension_search_list.sh
+++ Homework_Lesson11_BashShell_1/extension_search_list.sh
@@ -12,17 +12,17 @@ read -p "Введите расширение файлов, которое вам
 echo $extansion 
 
 # Переменные
-outputfile=$1
-catalog=$2
-extansion=$3
+#outputfile="$1"
+#catalog="$2"
+#extansion="$3"
 
 # Проверка, существует ли данный каталог
-if [ ! -d $catalog]; then
-    echo "Ошибка: Данный каталог '$catalog' не существует"
-    exit 1
-fi
+#if [ ! -d "$catalog"]; then
+#    echo "Ошибка: Данный каталог '$catalog' не существует"
+#    exit 1
+#fi
 
 # Поиск файлов в заданном каталоге, с заданным расширением и вывод его в outputfile
-find $catalog -type f -name *$extansion > $outputfile
+find "$catalog" -type f -name "*$extansion" > "$outputfile"
 
-echo "Список файлов с расширениями '$extansion' в файле '$outputfile'"
+echo "Список файлов с расширениями '$extansion' в файле '$outputfile'."
