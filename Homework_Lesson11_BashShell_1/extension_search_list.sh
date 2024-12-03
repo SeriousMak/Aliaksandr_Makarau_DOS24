e3cf28662c97410613b84979b2e99bc31784e5ab
SeriousMak <sashamkr@mail.ru>
2024-12-04 00:18:46 +0300

    feat:addding a extansion check

--- Homework_Lesson11_BashShell_1/extension_search_list.sh
+++ Homework_Lesson11_BashShell_1/extension_search_list.sh
@@ -2,25 +2,30 @@
 
 # Получение параметров
 
-read -p "Введите название выводящего(output) файла:" outputfile
-echo $outputfile
-
-read -p "Введите имя каталога:" catalog
-echo $catalog
-
-read -p "Введите расширение файлов, которое вам необходимо:" extansion
-echo $extansion 
-
 # Переменные
 #outputfile="$1"
 #catalog="$2"
 #extansion="$3"
 
-# Проверка, существует ли данный каталог
-#if [ ! -d "$catalog"]; then
-#    echo "Ошибка: Данный каталог '$catalog' не существует"
-#    exit 1
-#fi
+read -p "Введите название выводящего(output) файла: " outputfile
+echo $outputfile
+
+read -p "Введите имя каталога: " catalog
+echo $catalog
+
+# Проверка, существует ли введенный каталог
+#echo "Проверяем каталог: '$catalog'"
+if [ ! -d "$catalog" ]; then
+    echo "Ошибка: Данный каталог '$catalog' не существует"
+    exit 1
+fi
+
+read -p "Введите расширение файлов, которое вам необходимо: " extansion
+echo $extansion 
+if [[ "$extansion" != .* ]]; then
+    echo "Ошибка: расширение файла должно начинаться с точки ."
+    exit 1
+fi
 
 # Поиск файлов в заданном каталоге, с заданным расширением и вывод его в outputfile
 find "$catalog" -type f -name "*$extansion" > "$outputfile"
