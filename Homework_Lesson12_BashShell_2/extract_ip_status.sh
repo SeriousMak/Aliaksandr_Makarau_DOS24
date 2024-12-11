ce22c1b4d032cf88f1d4f30f1ba96dde2dba0a88
SeriousMak <sashamkr@mail.ru>
2024-12-11 13:55:12 +0300

    add echo and variables

--- Homework_Lesson12_BashShell_2/extract_ip_status.sh
+++ Homework_Lesson12_BashShell_2/extract_ip_status.sh
@@ -3,6 +3,12 @@
 #Генерация логов
 ./gen_server.sh
 
-grep 'status=200' logfile | grep -o 'ip=[^ ]*' | cut -d'=' -f2 | sort -u
+successful_ip=$(grep 'status=200' server.log | grep -o 'ip=[^ ]*' | cut -d'=' -f2 | sort -u)
 
-grep 'status=403' logfile | grep -o 'username=[^ ]*' | cut -d'=' -f2 | sort -u
+echo "Успешные IP-адреса status 200:"
+echo "$successful_ip"
+
+unsuccessful_users=$(grep 'status=403' server.log | grep -o 'user=[^ ]*' | cut -d'=' -f2 | sort -u)
+
+echo "Пользователи с неудачным входом status 403:"
+echo "$unsuccessful_users"
