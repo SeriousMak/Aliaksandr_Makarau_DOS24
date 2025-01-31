650f70c4f06b6fd2dd56aa388eaf5d4ee916108e
SeriousMak <sashamkr@mail.ru>
2025-01-31 19:10:47 +0300

    add 4 file.py Homework_Lesson23

---
+++ Homework_Lesson23_Python_1/multiplication_numer_9.py
@@ -0,0 +1,5 @@
+print("Введите два числа для их произведения")
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+result=x*y
+print(f"Умножение числа {x} на {y} равняется = {result}")
